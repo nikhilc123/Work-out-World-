900ecbf89ccb2e914955ed69b999b4527af17430
nikhilc <nikhilc.developer@gmail.com>
2016-03-02 21:09:02 +0000

    Add guard to an application

---
+++ spec/features/creating_homepage_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+RSpec.feature "Creating Home Page" do
+  scenario "Index page" do
+    visit "/"
+    
+    expect(page).to have_link "Athletes Nikhil"
+    expect(page).to have_link "Home"
+    expect(page).to have_content("Work out Lounge")
+  end
+end
