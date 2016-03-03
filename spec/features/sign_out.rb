4f3e774ad03175a73e6d86f41bbe05356d042f4e
nikhilc <nikhilc.developer@gmail.com>
2016-03-03 01:48:44 +0000

    sign out feature added with rspec

--- spec/features/sign_out.rb
+++ spec/features/sign_out.rb
@@ -1,13 +1,17 @@
-require 'rails_helper'
-RSpec.feature "Signed users should sign out" do
+require "rails_helper"
+RSpec.feature "Signing out signed-in users" do
   before do
     @bob = User.create!(email: "bob@gmail.com", password: "password")
-  end
-  
-  scenario "A user signs out" do
+    @nik = User.create!(email: "nik@gmail.com", password: "password")
     visit "/"
-    
-    click_link "Sign out"
+    click_link "Sign In"
+    fill_in "Email", with: @bob.email
+    fill_in "Password", with: @bob.password
+    click_button "Log in"
+  end
+  scenario do
+    visit "/"
+    click_link "Sign Out"
     expect(page).to have_content("Signed out successfully.")
   end
 end
