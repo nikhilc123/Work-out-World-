5516cb9e388b47c671e5bd6755cac9b08bff7d6e
nikhilc <nikhilc.developer@gmail.com>
2016-03-06 20:38:50 +0000

    added username to signup and created a new registrationcontrollers and whitelisted all the parameters required for sign up

--- spec/features/sign_up.rb
+++ spec/features/sign_up.rb
@@ -4,11 +4,14 @@ RSpec.feature "Create a sign up feature" do
     visit "/"
     
     click_link "Sign up"
+    fill_in "First name", with: "Nikhil"
+    fill_in "Last name", with: "Chikorde"
     fill_in "Email", with: "nik@gmail.com"
     fill_in "Password", with: "password"
     fill_in "Password confirmation", with: "password"
     click_button "Sign up"
     
     expect(page).to have_content("You have signed up successfully.")
+    expect(page).to have_content("Nikhil Chikorde")
   end
 end
